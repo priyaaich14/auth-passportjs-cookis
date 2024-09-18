0b65d09791b8f6b2ead02c3f731145d2f66ce770
priyaaich14 <80387848+priyaaich14@users.noreply.github.com>
2024-09-18 16:33:10 +0530

    Delete src directory

--- src/components/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom'
-export default function PrivateRoute(props) {
-    if(localStorage.getItem('token')) {
-        return props.children 
-    } else {
-        return <Navigate to="/login" />
-    }
-}
